ef052c5a1f8c4b1d0910c64e40451a3a0a8c500d
Parker C. Wilson, MD, PhD <53058914+p4rkerw@users.noreply.github.com>
2024-04-01 10:48:33 -0400

    Create impute.R

---
+++ impute.R
@@ -0,0 +1,10 @@
+#!/usr/bin/env Rscript
+# to run locally:
+# SCRATCH1=/mnt/g/scratch
+# docker run -it \
+# --workdir $HOME \
+# -v /mnt/g/diabneph:$HOME/project \
+# -v $HOME:$HOME \
+# -v $SCRATCH1:$SCRATCH1 \
+# -e SCRATCH1="/mnt/g/scratch" \
+# p4rkerw/sctools:R4.1.0
